dc171426802ca5617a5278cb04d7b30968c34a7e
Nick Baugh <niftylettuce@gmail.com>
2016-09-16 01:50:09 -0400

    Fixed #2 - set `ctx.body`

--- index.js
+++ index.js
@@ -3,55 +3,51 @@ var path = require('path');
 var debug = require('debug')('koa-nunjucks-promise');
 
 module.exports = function (root, opts) {
-    var basePath = path.dirname(module.parent.filename);
 
-    if (typeof root === 'object') {
-        opts = root;
-        root = opts.root || basePath;
-    } else if (!root) {
-        root = basePath;
-    }
+  var basePath = path.dirname(module.parent.filename);
 
-    root = path.resolve(basePath, root);
+  if (typeof root === 'object') {
+    opts = root;
+    root = opts.root || basePath;
+  } else if (!root) {
+    root = basePath;
+  }
 
-    opts = opts || {};
+  root = path.resolve(basePath, root);
 
-    var env = nunjucks.configure(root, opts);
+  opts = opts || {};
 
-    var ext = opts.ext || 'html';
+  var env = nunjucks.configure(root, opts);
 
-    var filters = opts.filters || {};
-    Object.keys(filters).forEach(f => {
-        env.addFilter.apply(env, [f].concat(filters[f]));
-    });
+  var ext = opts.ext || 'html';
 
-    var globals = opts.globals || {};
-    Object.keys(globals).forEach(g => {
-        env.addGlobal(g, globals[g]);
-    });
+  var filters = opts.filters || {};
+  Object.keys(filters).forEach(f => {
+    env.addFilter.apply(env, [f].concat(filters[f]));
+  });
 
-    return function (ctx, next) {
-        if (ctx.render) return next();
-        ctx.render = function (view, locals) {
-            locals = locals || {};
-            var state = ctx.state || {};
-            var context = Object.assign({}, state, locals);
+  var globals = opts.globals || {};
+  Object.keys(globals).forEach(g => {
+    env.addGlobal(g, globals[g]);
+  });
 
-            debug('render %s with %j', view, context);
-            return new Promise(function (resolve, reject) {
-                var name = view + '.' + ext;
-                env.render(name, context, function (err, res) {
-                    if (err) {
-                        reject(err);
-                    } else {
-                        resolve(res);
-                    }
-                });
-            }).then(function (html) {
-                ctx.body = html;
-            });
-        };
-        return next();
-    }
+  return function (ctx, next) {
+    if (ctx.render) return next();
+    ctx.render = function (view, locals) {
+      locals = locals || {};
+      var state = ctx.state || {};
+      var context = Object.assign({}, state, locals);
+      debug('render %s with %j', view, context);
+      return new Promise(function (resolve, reject) {
+        var name = view + '.' + ext;
+        env.render(name, context, function (err, res) {
+          if (err) return reject(err);
+          ctx.body = res;
+          resolve();
+        });
+      });
+    };
+    return next();
+  }
 };
 
